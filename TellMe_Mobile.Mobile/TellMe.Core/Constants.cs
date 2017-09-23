7474e0b1a9d248e6a1160ceac77bf62e86eff456
alex.v.minski <alex.v.minski@gmail.com>
2017-09-23 16:16:53 +0300

    fix api url

--- TellMe_Mobile.Mobile/TellMe.Core/Constants.cs
+++ TellMe_Mobile.Mobile/TellMe.Core/Constants.cs
@@ -2,6 +2,7 @@ namespace TellMe.Core
 {
     public class Constants
     {
-        public const string ApiHost = "http://localhost:5000";
+        //public const string ApiHost = "http://localhost:5000";
+        public const string ApiHost = "http://tellme-server.azurewebsites.net";
     }
 }
