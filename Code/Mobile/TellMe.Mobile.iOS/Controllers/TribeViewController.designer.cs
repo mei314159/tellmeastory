8f246171b04f5e943e1a4317a148986d7cf00abc
Vitaly Dubnovitsky <vitaly.dubnovitsky@gmail.com>
2018-11-12 11:33:59 +0300

    fix builld

--- Code/Mobile/TellMe.Mobile.iOS/Controllers/TribeViewController.designer.cs
+++ Code/Mobile/TellMe.Mobile.iOS/Controllers/TribeViewController.designer.cs
@@ -1,88 +1,78 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace TellMe.iOS.Controllers
 {
-    [Register ("TribeViewController")]
-    partial class TribeViewController
-    {
-        [Outlet]
-        UIKit.UILabel EventsCount { get; set; }
+	[Register ("TribeViewController")]
+	partial class TribeViewController
+	{
+		[Outlet]
+		UIKit.UILabel EventsCount { get; set; }
 
+		[Outlet]
+		UIKit.UIView HeaderView { get; set; }
 
-        [Outlet]
-        UIKit.UIView HeaderView { get; set; }
+		[Outlet]
+		UIKit.UILabel MembersCount { get; set; }
 
+		[Outlet]
+		UIKit.UIActivityIndicatorView Spinner { get; set; }
 
-        [Outlet]
-        UIKit.UILabel MembersCount { get; set; }
+		[Outlet]
+		UIKit.UILabel StoriesCount { get; set; }
 
+		[Outlet]
+		UIKit.UILabel TribeName { get; set; }
 
-        [Outlet]
-        UIKit.UIActivityIndicatorView Spinner { get; set; }
+		[Action ("BackButtonTouched:")]
+		partial void BackButtonTouched (Foundation.NSObject sender);
 
+		[Action ("InfoButtonTouched:")]
+		partial void InfoButtonTouched (Foundation.NSObject sender);
 
-        [Outlet]
-        UIKit.UILabel StoriesCount { get; set; }
+		[Action ("RequestStoryTouched:")]
+		partial void RequestStoryTouched (Foundation.NSObject sender);
 
+		[Action ("SendStoryTouched:")]
+		partial void SendStoryTouched (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (EventsCount != null) {
+				EventsCount.Dispose ();
+				EventsCount = null;
+			}
 
-        [Outlet]
-        UIKit.UILabel TribeName { get; set; }
+			if (MembersCount != null) {
+				MembersCount.Dispose ();
+				MembersCount = null;
+			}
 
+			if (Spinner != null) {
+				Spinner.Dispose ();
+				Spinner = null;
+			}
 
-        [Action ("BackButtonTouched:")]
-        partial void BackButtonTouched (Foundation.NSObject sender);
+			if (StoriesCount != null) {
+				StoriesCount.Dispose ();
+				StoriesCount = null;
+			}
 
+			if (TribeName != null) {
+				TribeName.Dispose ();
+				TribeName = null;
+			}
 
-        [Action ("InfoButtonTouched:")]
-        partial void InfoButtonTouched (Foundation.NSObject sender);
-
-
-        [Action ("RequestStoryTouched:")]
-        partial void RequestStoryTouched (Foundation.NSObject sender);
-
-
-        [Action ("SendStoryTouched:")]
-        partial void SendStoryTouched (Foundation.NSObject sender);
-
-        void ReleaseDesignerOutlets ()
-        {
-            if (EventsCount != null) {
-                EventsCount.Dispose ();
-                EventsCount = null;
-            }
-
-            if (HeaderView != null) {
-                HeaderView.Dispose ();
-                HeaderView = null;
-            }
-
-            if (MembersCount != null) {
-                MembersCount.Dispose ();
-                MembersCount = null;
-            }
-
-            if (Spinner != null) {
-                Spinner.Dispose ();
-                Spinner = null;
-            }
-
-            if (StoriesCount != null) {
-                StoriesCount.Dispose ();
-                StoriesCount = null;
-            }
-
-            if (TribeName != null) {
-                TribeName.Dispose ();
-                TribeName = null;
-            }
-        }
-    }
+			if (HeaderView != null) {
+				HeaderView.Dispose ();
+				HeaderView = null;
+			}
+		}
+	}
 }
